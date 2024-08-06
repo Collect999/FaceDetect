2630913890f980e121d69726281769d86caa5f9f
Collect999 <dregz299@gmail.com>
2024-08-06 07:34:00 +0100

    Initial Commit

---
+++ FaceDetectDemo/FaceDetectDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceDetectDemoApp.swift
+//  FaceDetectDemo
+//
+//  Created by Will Wade on 06/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct FaceDetectDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
